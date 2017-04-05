fadf43e8735e88616d60ae768be93eead44f40f0
TJ2001 <jung.m.tim@gmail.com>
2017-04-05 00:06:59 -0700

    add library

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,11 @@
 const gulp = require('gulp');
 const concat = require('gulp-concat');
 const browserSync = require('browser-sync').create();
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
+const uglify = require('gulp-uglify');
+const clean = require('gulp-clean-css');
+const del = require('del')
 
 const scripts = require('./scripts');
 const styles = require('./styles');
@@ -9,7 +14,11 @@ var devMode = false;
 
 gulp.task('css', function(){
   gulp.src(styles)
+      .pipe(sourcemaps.init())
+      .pipe(sass())
+      .pipe(sourcemaps.write())
       .pipe(concat('main.css'))
+      .pipe(clean())
       .pipe(gulp.dest('./dist/css'))
       .pipe(browserSync.reload({
           stream: true
@@ -19,11 +28,21 @@ gulp.task('css', function(){
 gulp.task('js', function(){
   gulp.src(scripts)
       .pipe(concat('scripts.js'))
+      .pipe(uglify())
       .pipe(gulp.dest('./dist/js'))
       .pipe(browserSync.reload({
         stream: true
       }));
 });
+
+gulp.task('copy', function () {
+    gulp.src('src/library/**/*')
+    .pipe(gulp.dest('./dist/library'))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
+});
+
 
 gulp.task('html', function(){
   gulp.src('./src/templates/**/*.html')
@@ -33,8 +52,16 @@ gulp.task('html', function(){
       }));
 });
 
+gulp.task('rebuild', function(){
+  del('dist');
+});
+
+gulp.task('destroy', function(){
+  del(['dist', 'bower_components'])
+})
+
 gulp.task('build', function(){
-  gulp.start(['css','js','html']);
+  gulp.start(['rebuild','copy','css','js','html']);
 });
 
 gulp.task('browserSync', function() {
