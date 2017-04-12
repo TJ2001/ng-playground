2fb65dd64dde4ba56ed5c178b7b9184b8b045266
TJ2001 <jung.m.tim@gmail.com>
2017-04-12 15:58:34 -0700

    fix responsive issue with carousel

--- src/js/controllers/HomeCtrl.js
+++ src/js/controllers/HomeCtrl.js
@@ -2,7 +2,6 @@ angular.module('sprout')
 .controller('HomeCtrl', [
     '$scope', '$location',
     function($scope, $location) {
-        console.log('Loaded.');
         $scope.message = 'Message';
         $scope.isActive = function(route) {
             return route === $location.path();
---
+++ src/js/directives/test2.js
@@ -0,0 +1,25 @@
+var myApp = angular.module('sprout');
+
+myApp.directive('marginSlide', function($parse){
+  return {
+    restrict: "E",
+    replace: true,
+    transclude: false,
+    compile: function (element, attrs) {
+
+      $(".project-back").hover(function(){
+          $(".shade").animate({
+              width: 'toggle'
+          }, 800);
+      });
+      $(".project-back2").hover(function(){
+          $(".shade2").animate({
+              width: 'toggle'
+          }, 800);
+      });
+      $(".project-back3").hover(function(){
+          $(".shade3").animate({
+              width: 'toggle'
+          }, 800);
+      });
+}}});
